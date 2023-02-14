250714fb19364428f5741bf7cad09e0c7495ac03
Ndubueze Kingsley Chinedu <117733203+ApstK@users.noreply.github.com>
2023-02-14 00:19:22 -0800

    Add files via upload

---
+++ TxE task 1.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+num1 = 5
+num2 = 7
+
+sum = num1 + num2
+
+print("The sum of {0} and {1} is {2}".format(num1, num2, sum))
+
+
+# In[4]:
+
+
+n = int("3")
+result = (n % i for i in range(2, n))
+
+if result:
+    print(n, "prime num")
+    
+        
+
+
+# In[ ]:
+
+
+
+        
+    
+
+
+# In[ ]:
+
+
+
+
